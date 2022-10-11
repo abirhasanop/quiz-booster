dd1313e9868614e51ab5fc4e50f65f3bf195d058
abirhasan <abir77077@gmail.com>
2022-10-11 21:21:39 +0600

    responsive container added

--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Statistics = () => {
     const data = useLoaderData()
     console.log(data.data)
     return (
         <div className='mx-auto w-1/2 mt-10'>
-            <LineChart width={600} height={500} data={data.data}>
+            <ResponsiveContainer width={100} height={100}>
+                <LineChart width={600} height={500} data={data.data}>
                 <XAxis dataKey="name" />
                 <YAxis dataKey="total" />
                 <Line type="monotone" dataKey="total" stroke="#82ca9d" />
                 <Tooltip/>
                 <Legend/>
             </LineChart>
+            </ResponsiveContainer>
+            
         </div>
     );
 };
